39b3633e4670639646383bbe4401927b47502c19
rachit2panwar <32370049+rachit2panwar@users.noreply.github.com>
2019-09-18 11:26:50 +0530

    Initial commit

---
+++ app/src/main/java/com/rachitpanwar/chatapp1/Model/Chatlist.java
@@ -0,0 +1,21 @@
+package com.rachitpanwar.chatapp1.Model;
+
+public class Chatlist {
+
+    public String id;
+
+    public Chatlist() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Chatlist(String id) {
+        this.id = id;
+    }
+}
